002a028c2d06614ccf390b9b64bd128cf27ac575
Tarasevich Vladislav <tarasevich.vlad.97@gmail.com>
2017-04-24 20:05:31 +0300

    Initialization of the Server class

---
+++ src/Server/Server.java
@@ -0,0 +1,16 @@
+package Server;
+
+/**
+ * Created by ReemBer on 24.04.2017.
+ * @author ReemBer
+ * @author gmail : tarasevich.vlad.97@gmail.com
+ * @version 1.0
+ * This class is a server.
+ */
+public class Server extends Thread
+{
+    public static void main(String args[])
+    {
+
+    }
+}
