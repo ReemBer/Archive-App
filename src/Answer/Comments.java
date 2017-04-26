c5d0b8c72ff883a1b0178fef66424ad37d70332f
Tarasevich Vladislav <tarasevich.vlad.97@gmail.com>
2017-04-26 14:10:36 +0300

    Created the Comments enumeration

---
+++ src/Answer/Comments.java
@@ -0,0 +1,11 @@
+package Answer;
+
+/**
+ * Created by Tarasevich Vladislav on 26.04.2017.
+ * @author ReemBer
+ * This enumeration is used to comment all server Answers
+ */
+public enum Comments
+{
+    OK, SERVER_ERROR, ACCESS_ERROR, NO_EXIST
+}
