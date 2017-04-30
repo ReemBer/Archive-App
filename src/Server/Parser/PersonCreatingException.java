4018bb74d8da4ea3ce19a37c3c7735293822e068
Tarasevich Vladislav <tarasevich.vlad.97@gmail.com>
2017-05-01 01:32:00 +0300

    added Exceptions, that used by all Person Parsers

---
+++ src/Server/Parser/PersonCreatingException.java
@@ -0,0 +1,7 @@
+package Server.Parser;
+
+/**
+ * Created by PC on 01.05.2017.
+ */
+public class PersonCreatingException {
+}
---
+++ src/Server/Parser/PersonParsingException.java
@@ -0,0 +1,6 @@
+package Server.Parser;
+
+/**
+ * Created by Tarasevich Vladislav on 01.05.2017.
+ */
+public class PersonParsingException extends Exception {}
