1202452a0aad00ebfa5ecfab31ead47fc6e7427c
Tarasevich Vladislav <tarasevich.vlad.97@gmail.com>
2017-04-30 15:20:27 +0300

    Initialization of the UserParser class, that used to creating and parsing User XML ducuments

---
+++ src/Server/Parser/UserParser.java
@@ -0,0 +1,23 @@
+package Server.Parser;
+
+import User.User;
+
+
+/**
+ * Created by Tarasevich Vladislav (ReemBer) on 30.04.2017.
+ * A class, that used to creating and parsing User XML files
+ */
+public class UserParser extends DOMXmlParser<User>
+{
+    @Override
+    public User parse(String xmlFile) throws Exception
+    {
+        return null;
+    }
+
+    @Override
+    public void create(User object) throws Exception
+    {
+
+    }
+}
