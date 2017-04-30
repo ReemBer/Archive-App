b5d481b9111dad39ad3a9a3c7beb142f43fff102
Tarasevich Vladislav <tarasevich.vlad.97@gmail.com>
2017-04-30 15:03:08 +0300

    Created abstract template class, that represent the Document Object Model

---
+++ src/Server/Parser/DOMXmlParser.java
@@ -0,0 +1,10 @@
+package Server.Parser;
+
+/**
+ * Created by Tarasevich Vladislav (ReemBer) on 30.04.2017.
+ * A class, that represents the Document Object Model
+ */
+public abstract class DOMXmlParser<PatternType> extends XmlParser<PatternType>
+{
+    public abstract void create(PatternType object) throws Exception;
+}
