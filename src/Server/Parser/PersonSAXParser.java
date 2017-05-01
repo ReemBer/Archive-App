90ddee9f2579bbc76d29c7197c1a5aa40bfa8342
Tarasevich Vladislav <tarasevich.vlad.97@gmail.com>
2017-05-01 13:13:47 +0300

    Initialized Person SAX Parser class

---
+++ src/Server/Parser/PersonSAXParser.java
@@ -0,0 +1,15 @@
+package Server.Parser;
+
+import PersonalData.Person;
+
+/**
+ * Created by Tarasevich Vladislav on 01.05.2017.
+ * 
+ */
+public class PersonSAXParser extends XmlParser<Person>
+{
+    @Override
+    public Person parse(String xmlFile) throws Exception {
+        return null;
+    }
+}
