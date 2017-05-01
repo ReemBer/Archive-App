521ae2dcb8c7805401b13f4cdb2863b9a8b508bb
Tarasevich Vladislav <tarasevich.vlad.97@gmail.com>
2017-05-02 00:34:55 +0300

    Added IllegalAccessException

---
+++ src/Server/RequestHandler/IllegalAccessException.java
@@ -0,0 +1,14 @@
+package Server.RequestHandler;
+
+/**
+ * Created by Tarasevich Vladislav on 01.05.2017.
+ */
+public class IllegalAccessException extends Exception
+{
+    public IllegalAccessException(){}
+
+    public IllegalAccessException(Throwable exception)
+    {
+        super(exception);
+    }
+}
